107099c8fc98d5495b7c94df5a02537bcd16c1d1
trestian-carmen-30127 <99726661+trestian-carmen-30127@users.noreply.github.com>
2023-07-28 16:31:33 +0300

    incercare de incarcat harta

--- my-app/src/app/about/about.component.ts
+++ my-app/src/app/about/about.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  title = 'locationApp';
+  @ViewChild('mapElement') mapElement!: ElementRef;
 
+  ngOnInit() {
+    this.loadGoogleMaps();
+    this.initMap();
+  }
+
+
+  loadGoogleMaps(): Promise<void> {
+    return new Promise<void>((resolve) => {
+      (window as any)['initGoogleMaps'] = () => {
+        resolve();
+      };
+
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_API_KEY&libraries=places&callback=initGoogleMaps`;
+      document.body.appendChild(script);
+    });
+  }
+
+
+  initMap() {
+    if (!navigator.geolocation) {
+      console.log('Geolocation is not supported');
+      return;
+    }
+
+    /*if (!google || !google.maps) {
+      console.log('Google Maps API not loaded');
+      return;
+    }*/
+
+    //se ia pozitia curenta si e setata ca centrul hartii
+    navigator.geolocation.getCurrentPosition((position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      const mapOptions = {
+        center: { lat: latitude, lng: longitude },
+        zoom: 14
+      };
+
+
+      const map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      // adauga marker la pozitia curenta 
+      const marker = new google.maps.Marker({
+        position: { lat: latitude, lng: longitude },
+        map: map,
+        title: 'You are here!'
+      });
+    });
+  }
+
+  getCurrentLocation() {
+    this.initMap();
+  }
 }
