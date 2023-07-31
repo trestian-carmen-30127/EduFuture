0ec0e36910a37696f5902b3039a8710887d792fa
trestian-carmen-30127 <99726661+trestian-carmen-30127@users.noreply.github.com>
2023-07-31 13:06:15 +0300

    Modificari harta

--- my-app/src/app/about/about.component.ts
+++ my-app/src/app/about/about.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 
+
+
 interface Position {
   coords: {
     latitude: number;
@@ -52,4 +54,5 @@ export class AboutComponent implements OnInit {
       console.error('Geolocation is not supported by this browser.');
     }
   }
+
 }
